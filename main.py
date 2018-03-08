ae19d1a6c5a0b54abf9de01708cc34f50becd320
Athisa Goh <tincank@gmail.com>
2018-03-07 16:21:55 -0800

    Custom template

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np #linear algebra
+import pandas as pd #CSV read
+
+#from sklearn - use scikit
+#from matplotlib import pyplot as plt - stats
+
+
+
+#######################################
+# Load files in from CSV using pandas #
+#######################################
+
+
+###########################################
+# Parse dataset for important information #
+###########################################
+
+
+###############
+# Train model #
+###############
+
+####################
+# Make predicition #
+####################
