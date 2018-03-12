1d43a4290ea2fd9557a5daa42cecab5e8b3d8382
AthisaV <tincank@gmail.com>
2018-03-12 10:21:40 -0700

    Actually broken

--- mainTest.py
+++ mainTest.py
@@ -1,5 +1,15 @@
 import numpy as np  #linear algebra
 import pandas as pd  #CSV read
+import tensorflow
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 ##### Load and Merging Dataset #####
 
@@ -68,7 +78,7 @@ train_test_outer = pd.merge( train.loc[ train['Season'].isin([2014, 2015, 2016, 
 train_test_outer = train_test_outer.loc[train_test_outer['Result'].isnull(),  ['TeamID1', 'TeamID2', 'Season']]
 
 train_test_missing = pd.merge( pd.merge( score_spread.loc[:, ['TeamID1', 'TeamID2', 'Season', 'Score_Ratio', 'Score_Pct']],train_test_outer, on = ['TeamID1', 'TeamID2', 'Season']),
-                              num_ot.loc[:, ['TeamID1', 'TeamID2', 'Season', 'NumOT']], on = ['TeamID1', 'TeamID2', 'Season'])
+num_ot.loc[:, ['TeamID1', 'TeamID2', 'Season', 'NumOT']], on = ['TeamID1', 'TeamID2', 'Season'])
 
 #scale data for keras
 X_test = train_test_missing.loc[:, ['Season', 'NumOT', 'Score_Ratio', 'Score_Pct']]
@@ -89,3 +99,46 @@ x_min = X_train.min()
 
 X_train = ( X_train - x_min ) / ( x_max - x_min + 1e-14)
 X_test = ( X_test - x_min ) / ( x_max - x_min + 1e-14)
+
+##### Generate #####
+
+seed = 8
+np.random.seed(seed)
+
+# create model
+model = Sequential()
+model.add(Dense(80, input_dim=8, kernel_initializer='lecun_normal', activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(60, kernel_initializer='lecun_normal', activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(50, kernel_initializer='lecun_normal', activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(45, kernel_initializer='lecun_normal', activation='relu'))
+model.add(Dropout(0.09))
+model.add(Dense(30, kernel_initializer='lecun_normal', activation='relu'))
+model.add(Dropout(0.12))
+model.add(Dense(15, kernel_initializer='lecun_normal', activation='relu'))
+model.add(Dropout(0.09))
+model.add(Dense(5, kernel_initializer='lecun_normal', activation='softmax'))
+model.add(Dropout(0.09))
+model.add(Dense(1, kernel_initializer='lecun_normal', activation='sigmoid'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.fit(X_train, train_labels, epochs=6000)
+
+##### Store #####
+
+train_test_inner['Pred1'] = model.predict_proba(X_train)
+train_test_missing['Pred1'] = model.predict_proba(X_test)
+
+sub = pd.merge(df_sample_sub, pd.concat( [train_test_missing.loc[:, ['Season', 'TeamID1', 'TeamID2', 'Pred1']], train_test_inner.loc[:, ['Season', 'TeamID1', 'TeamID2', 'Pred1']] ], axis = 0).reset_index(drop = True), on = ['Season', 'TeamID1', 'TeamID2'], how = 'outer')
+
+team1_probs = sub.groupby('TeamID1')['Pred1'].apply(lambda x : (x ** -1.0).mean() ** -1.0 ).fillna(0.5).to_dict()
+team2_probs = sub.groupby('TeamID2')['Pred1'].apply(lambda x : (x ** -1.0).mean() ** -1.0 ).fillna(0.5).to_dict()
+
+sub['Pred'] = sub[['TeamID1', 'TeamID2','Pred1']]\
+.apply(lambda x : team1_probs.get(x[0]) * ( 1 - team2_probs.get(x[1]) ) if np.isnan(x[2]) else x[2], axis = 1)
+
+sub[['ID', 'Pred']].to_csv('sub.csv', index = False)
+
+print(sub[['ID', 'Pred']].head(20))
